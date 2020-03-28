dbdd98eee1f354e294c82e02def76657ce532ec7
TitanPy <dani.g86@gmail.com>
2020-03-28 18:35:41 +0100

    notas actualizado

--- Backend/models/article.js
+++ Backend/models/article.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ArticleSchema = Schema({
-    title: String;
-    content: String;
+    title: String,
+    content: String,
     date: { type: Date, default: Date.now}, 
     image: String
 });
